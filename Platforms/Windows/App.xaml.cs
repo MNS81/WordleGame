44bc6e9599de9517abb5f532653ec0a553011218
Николай Матвеев <0x4c0656dc@gmail.com>
2025-03-31 21:48:35 +0300

    Добавьте файлы проекта.

---
+++ Platforms/Windows/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.UI.Windowing;
+using Microsoft.UI;
+using Microsoft.UI.Xaml;
+using Windows.Graphics;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WordleGame.WinUI
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : MauiWinUIApplication
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            base.OnLaunched(args);
+
+            var window = MauiWinUIApplication.Current.Application.Windows[0].Handler.PlatformView as Microsoft.UI.Xaml.Window;
+
+            var appWindow = AppWindow.GetFromWindowId(Win32Interop.GetWindowIdFromWindow(WinRT.Interop.WindowNative.GetWindowHandle(window)));
+            if (appWindow != null)
+            {
+                // Размер окна
+                int windowWidth = 600;
+                int windowHeight = 670;
+
+                appWindow.Resize(new SizeInt32(windowWidth, windowHeight));
+
+                // Получение размеров экрана
+                var displayInfo = DisplayArea.GetFromWindowId(appWindow.Id, DisplayAreaFallback.Primary);
+                var screenWidth = displayInfo.WorkArea.Width;
+                var screenHeight = displayInfo.WorkArea.Height;
+
+                // Расчет центра экрана
+                int positionX = (screenWidth - windowWidth) / 2;
+                int positionY = (screenHeight - windowHeight) / 2;
+
+                appWindow.Move(new PointInt32(positionX, positionY));
+            }
+        }
+    }
+
+}
---
+++ Words.cs
@@ -0,0 +1,50 @@
+﻿using BlazorBootstrap;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleGame
+{
+    internal class Words
+    {
+        public List<String> words = new List<String>();
+        [Inject] ModalService ModalService { get; set; } = default!;
+
+        public Words()
+        {
+            string path = Path.Combine(FileSystem.Current.AppDataDirectory, "words.db");   // Универсальный доступ к файлу
+            if (!File.Exists(path))     // Копирование файла из ресурсов, если он отсутствует в целевой директории
+            {
+                using var source = FileSystem.OpenAppPackageFileAsync("words.db").Result;
+                using var destination = File.Create(path);
+                source.CopyTo(destination);
+            }
+            if (File.Exists(path))  // Чтение файла
+            {
+                using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        words.Add(line.ToUpper().Replace("Ё", "Е"));
+                    }
+                }
+            }
+            else ShowModal(ModalType.Danger, "Файл words.db не найден.");
+        }
+
+        private async Task ShowModal(ModalType modalType, string msg)
+        {
+            var modalOption = new ModalOption
+            {
+                Title = "Внимание!",
+                Message = msg,
+                Type = modalType,
+            };
+            await ModalService.ShowAsync(modalOption);
+        }
+    }
+}
